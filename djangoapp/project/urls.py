a51bc5bae02cd3373e94dd223e664183f4d32514
oscarlmjr <oscarlmjr@gmail.com>
2026-05-09 17:16:19 -0300

    secao_12 aula_515_529

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -2,7 +2,7 @@
 URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,9 +17,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
-FROM python:3.14-alpine3.23
-LABEL mantainer="deck@deck.dev.br"
+FROM python:3.11.3-alpine3.18
+LABEL mantainer="oscarlmjr@gmail.com"
 
 # Essa variável de ambiente é usada para controlar se o Python deve 
 # gravar arquivos de bytecode (.pyc) no disco. 1 = Não, 0 = Sim
