1cce7e1251ed5261b65072faf5808d6b83a8bcc0
oscarlmjr <oscarlmjr@gmail.com>
2026-05-13 08:57:41 -0300

    secao_12 aula_542

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -53,3 +53,12 @@ class PostAdmin(admin.ModelAdmin):
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
+
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.updated_by = request.user  # type: ignore
+        else:
+            obj.created_by = request.user  # type: ignore
+
+        obj.save()
