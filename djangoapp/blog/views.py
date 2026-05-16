d06e8bf18457243ddabf4b35298a37db0c1084bc
oscarlmjr <oscarlmjr@gmail.com>
2026-05-16 19:56:33 -0300

    secao_12 aula_553-554

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,5 +1,6 @@
-from blog.models import Post
+from blog.models import Page, Post
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 
 PER_PAGE = 9
@@ -71,12 +72,42 @@ def tag(request, slug):
     )
 
 
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
+        }
+    )
+
+
 def page(request, slug):
+    page = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+
     return render(
         request,
         'blog/pages/page.html',
         {
-            # 'page_obj': page_obj,
+           'page': page,
         }
     )
 
