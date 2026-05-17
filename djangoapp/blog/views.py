fd3818f840078b9ee74f0aaa40e11f7a29382df1
oscarlmjr <oscarlmjr@gmail.com>
2026-05-17 16:00:29 -0300

    secao_12 aula_556-557

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,9 +4,31 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render
+from django.views.generic import ListView
 
 PER_PAGE = 9
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/pages/index.html'
+    context_object_name = 'posts'
+    ordering = '-pk',
+    paginate_by = PER_PAGE
+    queryset = Post.objects.get_published()
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(is_published=True)
+    #     return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'page_title': 'Home - ',
+            
+        })
+
+        return context
 
 def index(request):
     posts = Post.objects.get_published()
